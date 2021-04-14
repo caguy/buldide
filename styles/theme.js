e8acc0eecfb409af236dc74b6db384d0aeea19c5
caguy <camille.guy@gmail.com>
2021-04-14 18:27:52 +0200

    Basic global styling

---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+module.exports = {
+  colors: {
+    black: "black",
+    white: "white",
+    primary: {
+      DEFAULT: "#7655FE",
+      light: "#8567fe",
+    },
+  },
+  fontFamily: {
+    body: ["Heebo", "Roboto", "sans-serif"],
+  },
+  extend: {},
+};
